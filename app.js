cc4de0b0eeedadba5726f60ff2eb7b05c2dd3920
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-10-16 03:18:05 +0530

    feat: add functionality to check button

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 
-console.log("testing");
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check-number");
+
+checkButton.addEventListener("click",()=>{
+    console.log(dateOfBirth.value, luckyNumber.value);
+});
