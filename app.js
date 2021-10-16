0ee3ab5c2ea359e3a3364c569a859081b1d34d7b
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-10-16 07:00:52 +0530

    feat: add button function and input validation

--- app.js
+++ app.js
@@ -3,6 +3,35 @@ const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-number");
 
-checkButton.addEventListener("click",()=>{
-    console.log(dateOfBirth.value, luckyNumber.value);
-});
+const outputBox = document.querySelector("#output-box");
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let index=0; index<dob.length ; index++){
+        sum += Number(dob.charAt(index));
+    }
+
+    return sum;
+}
+
+function compareValues(sum,luckyNum){
+    const remainder = sum%luckyNum;
+    if(remainder === 0){
+        return "Your birthday is lucky 🙂"
+    }else{
+        return "Your birthday is not so lucky 😢"
+    }
+}
+
+function checkBirthDateIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob)
+    if(sum && luckyNumber.value){
+        outputBox.innerText = compareValues(sum,luckyNumber.value);
+    }else{
+        outputBox.innerText = "Please enter both the values."
+    }
+}
+
+checkButton.addEventListener("click",checkBirthDateIsLucky);
